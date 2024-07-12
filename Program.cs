fb717e33ca04de9a1642e5de7997353a0333fafc
muraoka2016 <felipemuraoka2016@outlook.com>
2024-07-12 01:21:02 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/* 1- Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+   2- Mostrar o conteúdo da variável curso no console.
+   3- Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+    Mostrar na tela o nome dos instrutores do curso.*/
+
+string curso = "Curso para iniciantes em C#";
+string nome = "Felipe";
+string sobrenome = "Muraoka";
+string instrutores = "Lecionado por: Guilherme e Daniel";
+
+Console.WriteLine(curso);
+Console.WriteLine(instrutores);
+Console.WriteLine(nome + " " + sobrenome);
+
